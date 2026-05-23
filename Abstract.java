0d24901bca63ceab74a3e7bbfb08f1c34fd42261
mstlaboniakter1433-wq <mstlaboniakter1433@gmail.com>
2026-05-23 20:13:23 +0600

    Abstraction

---
+++ Abstract.java
@@ -0,0 +1,18 @@
+abstract class Animal{
+     abstract void sound();
+      void sleep(){
+        System.out.println("This animal is sleeping");
+    }
+}
+class Dog extends Animal{
+    void sound(){
+        System.out.println("Dog barks");
+    }
+}
+public class Abstract{
+    public static void main(String[]args){
+        Dog d = new Dog();
+        d.sound();
+        d.sleep();
+    }
+}
