efac108c5f03ab067064b2871def5a083bf65721
mstlaboniakter1433-wq <mstlaboniakter1433@gmail.com>
2026-05-30 23:14:14 +0600

    Default Constructor

---
+++ Dconstructor.java
@@ -0,0 +1,12 @@
+class Cat {
+    Cat(){
+        System.out.println("Cat is eating");
+    }
+}
+public class Dconstructor {
+    public static void main(String[] args) {
+        Cat c = new Cat();
+        
+    }
+    
+}
