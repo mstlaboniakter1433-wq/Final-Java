09d2892e71ad0b1b9801f8e71a76027adbd64a6a
mstlaboniakter1433-wq <mstlaboniakter1433@gmail.com>
2026-06-01 23:52:39 +0600

    two-dimensiona Array)

---
+++ TwoDarray.java
@@ -0,0 +1,20 @@
+public class TwoDarray {
+    public static void main(String[] args) {
+
+        int[][] numbers = new int[2][3];
+
+        numbers[0][0] = 10;
+        numbers[0][1] = 20;
+        numbers[0][2] = 30;
+        numbers[1][0] = 40;
+        numbers[1][1] = 50;
+        numbers[1][2] = 60;
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(numbers[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
