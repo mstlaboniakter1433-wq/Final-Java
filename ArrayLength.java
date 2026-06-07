824fdd9aa4954470f6afb93ba56ca411a0fc66a5
mstlaboniakter1433-wq <mstlaboniakter1433@gmail.com>
2026-06-07 20:27:30 +0600

    Array Length Example

---
+++ ArrayLength.java
@@ -0,0 +1,7 @@
+public class ArrayLength{
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println("Array length = " + numbers.length);
+    }
+}
