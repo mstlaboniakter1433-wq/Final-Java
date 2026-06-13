065dba6bfc509d7abedb7d6f63d6330c43aff26e
mstlaboniakter1433-wq <mstlaboniakter1433@gmail.com>
2026-06-13 23:26:43 +0600

    Downcasting

---
+++ Downcasting.java
@@ -0,0 +1,18 @@
+class Animal{
+    void sound(){
+        System.out.println("Animal makes a sound");
+    }
+}
+class Dog extends Animal{
+    void bark(){
+        System.out.println("Dog barks");
+    }
+}
+public class Downcasting{
+    public static void main(String[] args) {
+
+        Animal a = new Dog(); // Upcasting
+        Dog d = (Dog) a; // Downcasting
+        d.bark();
+    }
+}
